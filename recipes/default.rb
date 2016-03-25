946f9a96485732bac02113068761e74554a588cf
nicosmuts <nico@smuts.me>
2016-03-25 19:01:17 +0200

    Update default.rb to run yum updates

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,12 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+
+
+execute "yum clean" do
+ command "yum clean all"
+end
+
+execute "yum update" do
+ command "yum -y update"
+end
