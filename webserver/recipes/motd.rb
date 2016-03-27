aed03d236e44aeab581ac5de9315349484c2a1a0
Nico <nico@smuts.me>
2016-03-27 10:58:19 +0200

    Update motd.rb

--- webserver/recipes/motd.rb
+++ webserver/recipes/motd.rb
@@ -1,10 +1,14 @@
+stack = search("aws_opsworks_stack").first
+layer = search("aws_opsworks_layer").first
+instance = search("aws_opsworks_instance", "self:true").first
+
 template "/etc/motd.opsworks-static" do
   source "motd.erb"
   mode "0644"
   variables({
-    :stack => node[:opsworks][:stack],
-    :layers => node[:opsworks][:layers],
-    :instance => node[:opsworks][:instance],
+    :stack => stack['name'],
+    :layers => layer['name'],
+    :instance => instance['instance_id'],
     :os_release => os_release
   })
 end
