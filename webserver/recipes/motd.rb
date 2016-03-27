43c1a3a23dc2fa7bc3eb959cac2a4a72e21cbdda
Nico <nico@smuts.me>
2016-03-27 13:52:17 +0200

    Update motd.rb

--- webserver/recipes/motd.rb
+++ webserver/recipes/motd.rb
@@ -1,9 +1,12 @@
+instance = search("aws_opsworks_instance", "self:true").first
+
 template "/etc/motd.opsworks-static" do
   source "motd.erb"
   mode "0644"
   variables({
     :stack => search("aws_opsworks_stack").first,
     :layers => search("aws_opsworks_layer").first,
-    :instance => search("aws_opsworks_instance", "self:true").first
+    :layer_names => instance[:layer_ids].map{|my_layer_ids| search(:aws_opsworks_layers, "layer_id:#{my_layer_id}").first}.join(', '),
+    :instance => instance
   })
 end
